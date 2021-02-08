1eec4862a4ef166f036b07a98822ff8f963b04a9
Austin Theriault <atheriault@gwu.edu>
2021-02-08 15:25:49 -0500

    Update tmp.c

--- tmp.c
+++ tmp.c
@@ -1,12 +1,18 @@
-/* [[file:quiz-c2-2.org::*Question 3][Question 3:3]] */
-/* [[[[file:~/2113/quiz-writeups/quiz-c2-2.org::struct][struct]]][struct]] */
+
+
+/* #+RESULTS: struct */
+
+/*  /explain struct here/ */
+
+/*  Now let's look at what's in main: */
+
+
 #include <stdio.h>
 #include <string.h>
   typedef struct{
     char ** value;
     char * another_value;
   }thing_t;
-/* struct ends here */
 int main() {
   char * string1 = "a b c ";
   char ** string_ptr = &string1;
@@ -20,4 +26,3 @@ int main() {
     printf("Go Away!!");
   }
 }
-/* Question 3:3 ends here */
